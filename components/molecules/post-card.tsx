73aae5c90f0dc51914c1d866ba52ee966d2b0523
onikun94 <onikun94@gmail.com>
2021-10-05 20:28:17 +0900

    change type name

--- components/molecules/post-card.tsx
+++ components/molecules/post-card.tsx
@@ -1,54 +1,69 @@
 import styled from "@emotion/styled";
-import { VFC } from "react";
+import React, { VFC } from "react";
 import { colors } from "../../styles/color";
 import { Typography } from "../atoms/typography";
 import Link from "next/link";
+import { Divider } from "../atoms/divider";
+
 export type PostCardPropType = {
   onClick: () => void;
   cardKey: number;
-  user: string;
-  address: string;
-  segment: string;
-  contents: string;
-  answer: string;
-  on_air: string;
-  className?: string;
+  thumbnail: string;
+  detail1: string;
+  detail2?: string;
+  detail3?: string;
+  detail4?: number;
+  maintext1: string;
+  maintext2?: string;
+  height?: number;
 };
 
 export const PostCard: VFC<PostCardPropType> = ({
   onClick,
   cardKey,
-  user,
-  address,
-  segment,
-  contents,
-  answer,
-  on_air,
+  thumbnail,
+  detail1,
+  detail2,
+  detail3,
+  detail4,
+  maintext1,
+  maintext2,
+  height,
 }) => {
   return (
     <Link href="detail">
-      <Container key={cardKey} onClick={onClick}>
+      <Container key={cardKey} onClick={onClick} height={height}>
         <Thumbnail>
-          <Typography text={segment} type="14bold"></Typography>
+          <Typography text={thumbnail} type="14bold"></Typography>
         </Thumbnail>
         <SideText>
           <Detail>
-            <Typography text={on_air} type="14normal" />
-            <Typography text={address} type="14normal" />
+            <Typography text={detail1} type="14normal" />
+            <Typography text={detail2} type="14normal" />
+            <Typography text={detail3} type="14normal" />
+            {detail4 ? (
+              <Typography text={String(detail4)} type="14normal" />
+            ) : (
+              <Typography text={""} type="14normal" />
+            )}
+
             {/* <Typography text={segment} type="14normal" />
           <Typography text={contents} type="14normal" />
           <Typography text={answer} type="14normal" /> */}
           </Detail>
-          <User>
-            <Typography text={user} type="20bold" />
-          </User>
+          <MainText>
+            <Typography text={maintext1} type="20bold" />
+          </MainText>
+          <MainText>
+            <Typography text={maintext2} type="20bold" />
+          </MainText>
         </SideText>
       </Container>
     </Link>
   );
 };
 
-const Container = styled.div({
+const Container = styled.div<{ height: number }>((props) => ({
   // display: "table-cell",
   cursor: "pointer", //カーソルを当てたときの変化
   verticalAlign: "middle",
@@ -57,12 +72,12 @@ const Container = styled.div({
   margin: "15px 5px",
   boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.15)",
   width: "400px",
-  height: "250px",
+  height: `${props.height}px` ?? "300px",
   justifyContent: "center",
   alignItems: "center",
   flexDirection: "column",
   transition: ".3s",
-});
+}));
 
 const Thumbnail = styled.div({
   textAlign: "center",
@@ -81,7 +96,7 @@ const SideText = styled.div({
   display: "block",
 });
 
-const User = styled.div({
+const MainText = styled.div({
   padding: "10px",
 });
 
