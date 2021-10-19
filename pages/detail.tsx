f8208870ee02e0b7326c3b3fe8ec0a143db64e42
onikun94 <onikun94@gmail.com>
2021-10-19 16:00:01 +0900

    change how to rendering

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { VFC } from "react";
 import styled from "@emotion/styled";
 import Link from "next/link";
 import { getYato } from "../api/getShimo";
-import { PostList } from "../components/templates/post-list";
 import { YatoList } from "../components/templates/yato-list";
+import { YatoType } from "../types/yato";
 
-const TopPage = () => {
-  const [yatos, setYatos] = useState([]);
-  const handleLog = () => {
-    console.log("button is pushed");
-  };
-  // // // 後にSSRに変換する
-  useEffect(() => {
-    (async () => {
-      const res = await getYato();
-      console.log("res is ");
-      console.log(res);
-      setYatos(res);
-    })();
-  }, []);
+export type DetailPagePropType = {
+  response: YatoType[];
+};
+
+const DetailPage: VFC<DetailPagePropType> = ({ response }) => {
   return (
     <MainText>
       <Link href="/">
         <a>home</a>
       </Link>
-      <YatoList yatoInfo={yatos} />
+      <YatoList yatoInfo={response} />
     </MainText>
   );
 };
 
+export const getServerSideProps = async () => {
+  const response = await getYato();
+  return {
+    props: { response },
+  };
+};
+
 const MainText = styled.div({});
 
-export default TopPage;
+export default DetailPage;
