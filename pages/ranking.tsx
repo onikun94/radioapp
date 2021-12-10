3f1b9f6f2b89046e8690439a27d6cd20941f3dc4
onikun94 <onikun94@gmail.com>
2021-12-10 11:00:12 +0900

    add header

--- pages/ranking.tsx
+++ pages/ranking.tsx
@@ -1,6 +1,8 @@
+import styled from "@emotion/styled";
 import Link from "next/link";
 import React, { VFC } from "react";
 import { getYato } from "../api/getShimo";
+import { Header } from "../components/molecules/header";
 import { RankingList } from "../components/templates/ranking-list";
 import { YatoType } from "../types/yato";
 
@@ -11,11 +13,19 @@ type rankingPagePropType = {
 const RankingPage: VFC<rankingPagePropType> = ({ response }) => {
   return (
     <div>
-      <Link href="/">home</Link>
+      <HeaderContainer>
+        <Header
+          headerText="霜降りデータベース"
+          colorType="dark"
+          textType="24bold"
+        ></Header>
+      </HeaderContainer>
       <RankingList rankingData={response} />
     </div>
   );
 };
+
+const HeaderContainer = styled.div({});
 
 export const getServerSideProps = async () => {
   const response = await getYato();
