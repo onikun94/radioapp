e59cc1c6445f355f7e0c664b314f69de83804474
onikun94 <onikun94@gmail.com>
2021-12-10 11:00:54 +0900

    add header and displau userName

--- pages/users/[name].tsx
+++ pages/users/[name].tsx
@@ -1,10 +1,10 @@
+import styled from "@emotion/styled";
 import { GetServerSideProps } from "next";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState, VFC } from "react";
 import { searchUser } from "../../api/getShimo";
+import { Header } from "../../components/molecules/header";
 import { PostList } from "../../components/templates/post-list";
-import { YatoList } from "../../components/templates/yato-list";
 import { YatoType } from "../../types/yato";
 
 export type UserPostPropType = {
@@ -13,18 +13,26 @@ export type UserPostPropType = {
 
 const UserPost: VFC<UserPostPropType> = ({ userdata }) => {
   const router = useRouter();
-  const pname = router.query.name;
+  const pname = router.query.name.toString(); //nameは[name].tsxから
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Link href="/">home</Link>　<p>{pname}</p>
-      <PostList yatoInfo={userdata} />
+      <HeaderContainer>
+        <Header
+          headerText="霜降りデータベース"
+          colorType="dark"
+          textType="24bold"
+        ></Header>
+      </HeaderContainer>
+      {/* <p>{pname}</p> */}
+      <PostList userName={pname} yatoInfo={userdata} />
     </>
   );
 };
 
-// this serversideprops will be removed
+const HeaderContainer = styled.div({});
+
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const userdata = await searchUser(context.query.name);
   return {
