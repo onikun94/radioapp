56c0a0ee289f13917cda3c04c96787418bff0f8f
onikun94 <onikun94@gmail.com>
2021-12-10 10:55:21 +0900

    add link to header

--- components/molecules/header.tsx
+++ components/molecules/header.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Typography, TypographyType } from "../atoms/typography";
 import styled from "@emotion/styled";
 import { colors } from "../../styles/color";
+import { DataLink } from "../atoms/data-link";
 
 type HeaderColorType = "light" | "dark";
 
@@ -29,6 +30,16 @@ export const Header: React.VFC<HeaderPropType> = ({
           color={colorType === "dark" ? "white" : "primary"}
         />
       </HeaderText>
+      <LinkContainer>
+        <DataLink linkdata={"/"} contents="ホーム" textColor="white" />
+        <DataLink linkdata={"/users"} contents="ユーザ一覧" textColor="white" />
+        <DataLink linkdata={"/search"} contents="探す" textColor="white" />
+        <DataLink
+          linkdata={"/ranking"}
+          contents="ランキング"
+          textColor="white"
+        />
+      </LinkContainer>
     </HeaderContainer>
   );
 };
@@ -39,12 +50,23 @@ const HeaderContainer = styled.header<{ colorType: HeaderColorType }>(
       props.colorType === "dark"
         ? colors.background.dark
         : colors.background.primary,
-    padding: "20px",
-    display: "flex",
+    padding: "30px",
+    // display: "flex",
     justifyContent: "left",
-    alignItems: "left",
+    // alignItems: "left",
     borderBottom: `2px solid ${colors.border.gray}`,
   })
 );
 
-const HeaderText = styled.div();
+const LinkContainer = styled.div({
+  display: "flex",
+  float: "right",
+  // justifyContent: "right",
+  // flexWrap: "wrap",
+});
+
+const HeaderText = styled.div({
+  float: "left",
+  display: "flex",
+  // flexWrap: "wrap",
+});
