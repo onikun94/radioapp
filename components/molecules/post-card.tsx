c3ce1db89f810eab80411238dd606160be30162c
onikun94 <onikun94@gmail.com>
2021-12-10 10:55:54 +0900

    change card color

--- components/molecules/post-card.tsx
+++ components/molecules/post-card.tsx
@@ -2,8 +2,6 @@ import styled from "@emotion/styled";
 import React, { VFC } from "react";
 import { colors } from "../../styles/color";
 import { Typography } from "../atoms/typography";
-import Link from "next/link";
-import { Divider } from "../atoms/divider";
 
 export type PostCardPropType = {
   onClick: () => void;
@@ -41,14 +39,10 @@ export const PostCard: VFC<PostCardPropType> = ({
           <Typography text={detail2} type="14normal" />
           <Typography text={detail3} type="14normal" />
           {detail4 ? (
-            <Typography text={String(detail4)} type="14normal" />
+            <Typography text={String(detail4) + "pt"} type="14normal" />
           ) : (
             <Typography text={""} type="14normal" />
           )}
-
-          {/* <Typography text={segment} type="14normal" />
-          <Typography text={contents} type="14normal" />
-          <Typography text={answer} type="14normal" /> */}
         </Detail>
         <MainText>
           <Typography text={maintext1} type="20bold" />
@@ -66,10 +60,11 @@ const Container = styled.div<{ height: number }>((props) => ({
   cursor: "pointer", //カーソルを当てたときの変化
   verticalAlign: "middle",
   backgroundColor: colors.background.alphaPrimary,
-  ":hover": { backgroundColor: colors.background.dark },
+  ":hover": { backgroundColor: colors.background.accent },
   margin: "15px 5px",
-  boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.15)",
+  boxShadow: "0px 2px 4px 0px rgba(0, 0, 0, 0.15)",
   width: "400px",
+  borderRadius: "8px",
   height: `${props.height}px` ?? "300px",
   justifyContent: "center",
   alignItems: "center",
